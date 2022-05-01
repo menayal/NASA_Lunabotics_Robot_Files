4dad31fa00a8b63c169500f10bacabd39729f0f7
nayal_ubuntu <menayal1999@gmail.com>
2022-05-01 14:30:42 -0500

    updated script location to get absolute path

--- scripts/controllerScripts/initializeController.py
+++ scripts/controllerScripts/initializeController.py
@@ -5,6 +5,9 @@ import os
 #maybe check if the controller is conncted. 
 def initializeController():
     print("The controller is being initialized...")
-    os.system("bash runJoy.sh")
+    #os.system("bash runJoy.sh") #needed to be in dir to run
 
-
+    #runs file from root dir
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+    DATA_PATH = os.path.join(BASE_DIR, "runJoy.sh")
+    os.system("bash " + str(DATA_PATH))
